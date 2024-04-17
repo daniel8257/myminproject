f3dde69b36e58d3d63ab480307a2ee17607abdea
Pavel Pliuiko <pavelpliuiko@gmail.com>
2024-04-17 20:42:20 -0230

    passing params fix

--- booking.js
+++ booking.js
@@ -1,31 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const bookingForm = document.getElementById('bookingForm');
     const totalPriceInput = document.getElementById('totalPrice');
     const checkoutBtn = document.getElementById('checkoutBtn');
-    
+    // Pavel's comment: add missing variables
+    const checkoutLink = document.getElementById('checkoutLink');
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+
+    // Pavel's comment: very strange function I suggest to use "FormData"
     // Function to calculate total price
     const calculateTotalPrice = () => {
         console.log('Calculating total price...');
         let totalPrice = 0;
         const numRoomsInputs = document.querySelectorAll('.num-rooms');
         const priceInputs = document.querySelectorAll('.price');
-        
+
         numRoomsInputs.forEach((numRoomsInput, index) => {
             const numRooms = parseInt(numRoomsInput.value);
             const price = parseInt(priceInputs[index].value.slice(1));
             console.log(`Rooms: ${numRooms}, Price: ${price}`);
             totalPrice += numRooms * price;
         });
-        
+
         totalPriceInput.value = `$${totalPrice}`;
+
+        // Pavel's comment: you will use "totalPrice" when you will create search params
+        return totalPrice
     };
-    
+
+    // Pavel's comment: function for creating search params
+    const createSearchParams = ({name, email, phone, total}) => {
+        const params = new URLSearchParams();
+        params.append('name', name)
+        params.append('email', email)
+        params.append('phone', phone)
+        params.append('total', total)
+
+        return params
+    }
+
     // Event listeners
-    bookingForm.addEventListener('submit', function(event) {
+    bookingForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        calculateTotalPrice();
+
+        const totalPrice = calculateTotalPrice();
+        // Pavel's comment: create search params from your input values and total price
+        const searchParams = createSearchParams({
+            name: nameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value,
+            total: totalPrice
+        })
+
         checkoutBtn.style.display = 'block';
+        // Pavel's comment: modify your link href to pass search params to checkout page
+        checkoutLink.href = `./checkout.html?${searchParams.toString()}`
     });
-    
+
     bookingForm.addEventListener('change', calculateTotalPrice);
 });
--- checkout.js
+++ checkout.js
@@ -1,25 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Pavel's comment: you dont need it
     // Function to retrieve query parameters from the URL
-    const getUrlParameter = (name) => {
-        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-        const results = regex.exec(location.search);
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-    };
+    // const getUrlParameter = (name) => {
+    //     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    //     const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    //     const results = regex.exec(location.search);
+    //     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    // };
 
+    // Pavel's comment: you can just use URLSearchParams instead of your logic in getUrlParameter function
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Pavel's comment: you should use urlParams instead of commented getUrlParameter function
     // Retrieve and display name, email, phone, and total amount
-    const name = getUrlParameter('name');
-    const email = getUrlParameter('email');
-    const phone = getUrlParameter('phone');
-    const total = getUrlParameter('total');
+    const name = urlParams.get('name') //getUrlParameter('name');
+    const email = urlParams.get('email') //getUrlParameter('email');
+    const phone = urlParams.get('phone') //getUrlParameter('phone');
+    const total = urlParams.get('total') //getUrlParameter('total');
 
-    document.getElementById('name').innerText = name;
-    document.getElementById('email').innerText = email;
-    document.getElementById('phone').innerText = phone;
-    document.getElementById('total').innerText = total;
+    // Pavel's comment: you make a mistake in ids
+    document.getElementById('checkoutName').innerText = name;
+    document.getElementById('checkoutEmail').innerText = email;
+    document.getElementById('checkoutPhone').innerText = phone;
+    document.getElementById('checkoutTotalPrice').innerText = total;
 
+    // Pavel's comment: I didn't find this inputs in your html
     // Populate hidden input fields with name, email, and phone
-    document.getElementById('nameInput').value = name;
-    document.getElementById('emailInput').value = email;
-    document.getElementById('phoneInput').value = phone;
+    // document.getElementById('nameInput').value = name;
+    // document.getElementById('emailInput').value = email;
+    // document.getElementById('phoneInput').value = phone;
 });
